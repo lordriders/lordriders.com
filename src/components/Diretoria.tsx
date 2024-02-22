b2af362d3b86720c6da5b3d72cd269da090f2095
lordriders <132901671+lordriders@users.noreply.github.com>
2024-02-22 17:57:31 -0300

    Update Diretoria.tsx

--- src/components/Diretoria.tsx
+++ src/components/Diretoria.tsx
@@ -7,7 +7,7 @@ export default function Diretoria() {
           <div className="col-md-3 text-center">
             <picture className="figure">
               <img
-                src="/media/figs/diretoria/presi.webp"
+                src="/media/figs/diretoria/Presi2024"
                 className="figure-img img-fluid diretor"
                 alt="Presidente"
               />
@@ -19,7 +19,7 @@ export default function Diretoria() {
           <div className="col-md-3 text-center">
             <picture className="figure">
               <img
-                src="/media/figs/diretoria/Presi2024.jpg"
+                src="/media/figs/diretoria/vip.webp.jpg"
                 className="figure-img img-fluid diretor"
                 alt="Vice Presidente"
               />
