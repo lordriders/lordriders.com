231260926499fe4bf1894cde794e6c7bb88c65d4
lordriders <132901671+lordriders@users.noreply.github.com>
2024-02-23 15:16:00 -0300

    Update Diretoria.tsx

--- src/components/Diretoria.tsx
+++ src/components/Diretoria.tsx
@@ -75,7 +75,6 @@ export default function Diretoria() {
             <strong>Rose</strong>
             <p className="text-muted">(Rosilene Miskalo)</p>
           </div>
-          </div>
         </div>
       </div>
       <a href="#top-bar" className="back-top a-dark">
