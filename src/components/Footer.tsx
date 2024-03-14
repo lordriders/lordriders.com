a1bc2d2585545eec739a97e1083e9c30ebe3dd63
lordriders <132901671+lordriders@users.noreply.github.com>
2024-03-14 09:02:48 -0300

    Atualizar o Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -136,21 +136,7 @@ export default function Footer() {
         </a>
       </section>
       <footer className="footer text-muted">
-        Feito com &nbsp;{" "}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-heart-fill"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-          />
-        </svg>{" "}
-        &nbsp; por Cavalo de Aço Sistemas
+        Feito por Cavalo de Aço Sistemas
       </footer>
       <div id="gallery-modal" className="modal">
         <span className="close" id="close-modal">
